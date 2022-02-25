160a0ef839ef4271ba0d911eeb59dfe16424e521
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-02-25 15:00:23 -0600

    Se agrega archivo main.js para agregar funciones de uso parametrizado.

---
+++ src/Presentation.WebApp/wwwroot/js/main.js
@@ -0,0 +1,24 @@
+﻿const consumirMetodoAccion = (pUrl, pAsync, pType, pData, funDone, funFail = undefined, funAlways = undefined) => {
+    $.ajax({
+        url: location.origin + pUrl,
+        data: pData,
+        async: pAsync,
+        type: pType
+    })
+
+        .done(funDone)
+
+        .fail(funFail)
+
+        .always(funAlways);
+}
+
+function ocultar() {
+
+    let x = document.querySelector("#header");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
--- src/Presentation.WebApp/wwwroot/js/site.js
+++ src/Presentation.WebApp/wwwroot/js/site.js
@@ -121,7 +121,7 @@ function details(controller, id) {
 $(document).ready(function () {
 
 
-  
+
     $.ajax({
         type: "POST",
         url: location.origin + "/Citas/ListarCitas",
@@ -131,15 +131,15 @@ $(document).ready(function () {
         success: OnSuccess,
         async: false,
         failure: function (response) {
-           
+
         },
         error: function (response) {
-            
+
         }
     });
 
-    
-    
+
+
 });
 
 var exampleModal = document.getElementById('exampleModal')
@@ -183,29 +183,29 @@ function listarCitas() {
 function OnSuccess(response) {
 
     $('#citasTabla').DataTable().clear().destroy();
-  
-     
+
+
     $('#citasTabla').DataTable({
         "ordering": true,
         "searching": true,
         "lengthChange": true,
-      
+
         "pageLength": 10,
         processing: true,
-  
+
         deferRender: true,
         scrollY: 500,
         scrollCollapse: true,
         scroller: true,
         async: true,
-    
+
         data: response,
         columns: [
             { 'data': 'id' },
             { 'data': 'nombrePaciente' },
             { 'data': 'estatusCita' },
             { 'data': 'areaAtencion' },
-            { 'data': 'fecha'},
+            { 'data': 'fecha' },
             {
                 mRender: function (data, type, row) {
                     return '<a href="#' + row.id + '" class="btn btn-danger m-1" onclick="eliminar(this)">Cancelar</a><a data-bs-toggle="modal" data-bs-target="#exampleModal2" data-bs-whatever="mdo"  href="#' + row.id + '" class="btn btn-info m-1" onclick="detalleCita(this)">Detalle</a><a class="btn btn-primary m-1" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="mdo" onclick="editarCita(this)" href="#' + row.id + '">Editar</a >'
@@ -266,45 +266,45 @@ function OnSuccess(response) {
             "url": "//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json"
         }
     });
-    
-   
-    
-    
+
+
+
+
 }
 
 const eliminar = (idCitaElimiar) => {
     console.log(idCitaElimiar.data);
     alertify.confirm('Cancelar cita', 'A continuación cancelará esta cita, desea continuar?',
         function () {
-            
-             
-                $.ajax({
-                    url: location.origin + "/Citas/CancelarCita", // Url
-                    data: {
-                        id: idCitaElimiar.hash.replace('#', '')
-                        // ...
-                    },
-                    async: false,
-                    type: "post"  // Verbo HTTP
+
+
+            $.ajax({
+                url: location.origin + "/Citas/CancelarCita", // Url
+                data: {
+                    id: idCitaElimiar.hash.replace('#', '')
+                    // ...
+                },
+                async: false,
+                type: "post"  // Verbo HTTP
+            })
+                // Se ejecuta si todo fue bien.
+                .done(function (result) {
+                    if (result != null) {
+                        alertify.success('La cita se canceló correctamente');
+                        listarCitas();
+                    }
+                    else {
+                    }
                 })
-                    // Se ejecuta si todo fue bien.
-                    .done(function (result) {
-                        if (result != null) {
-                            alertify.success('La cita se canceló correctamente');
-                            listarCitas();
-                        }
-                        else {
-                        }
-                    })
-                    // Se ejecuta si se produjo un error.
-                    .fail(function (xhr, status, error) {
+                // Se ejecuta si se produjo un error.
+                .fail(function (xhr, status, error) {
 
-                    })
-                    // Hacer algo siempre, haya sido exitosa o no.
-                    .always(function () {
+                })
+                // Hacer algo siempre, haya sido exitosa o no.
+                .always(function () {
 
-                    });
-            
+                });
+
 
         }
         , function () {
@@ -315,8 +315,8 @@ const eliminar = (idCitaElimiar) => {
 
 
 const detalleCita = (cita) => {
-    
-    $.ajax({
+
+    /*$.ajax({
         url: location.origin + "/Citas/Details", // Url
         data: {
             id: cita.hash.replace('#', '')
@@ -337,8 +337,8 @@ const detalleCita = (cita) => {
                         <tr><td class="p-1">Área de atención:</td><td class="p-1"><strong>${result.areaAtencion}</strong></td></tr>
                     </tbody>`
 
-                
-                
+
+
             }
             else {
             }
@@ -350,7 +350,25 @@ const detalleCita = (cita) => {
         // Hacer algo siempre, haya sido exitosa o no.
         .always(function () {
 
-        });
+        });*/
+    consumirMetodoAccion("/Citas/Details", false, "post", { id: cita.hash.replace('#', '') }, (result) => {
+
+        if (result != null) {
+
+            document.querySelector("#tablaDetalleCita").innerHTML =
+                `<tbody >
+                        <tr><td class="p-1">Identificador del paciente:</td><td class="p-2"><strong>${result.id}</strong></td></tr>
+                        <tr><td class="p-1">Nombre del paciente:</td><td class="p-2"><strong>${result.nombrePaciente}</strong></td></tr>
+                        <tr><td class="p-1">Fecha de la cita:</td><td class="p-2"><strong>${new Date(result.fecha).toLocaleString()}</strong></td></tr>
+                        <tr><td class="p-1"> Estatus de cita:</td><td class="p-1"><strong>${result.estatusCita}</strong></td></tr>
+                        <tr><td class="p-1">Área de atención:</td><td class="p-1"><strong>${result.areaAtencion}</strong></td></tr>
+                    </tbody>`
+
+
+
+        }
+
+    });
 
 
 }
@@ -422,7 +440,7 @@ const editarCita = (cita) => {
 
                 $("#areatencion option:contains(" + result.areaAtencion + ")").attr('selected', true);
                 $("#estatus option:contains(" + result.estatusCita + ")").attr('selected', true);
-               
+
             }
             else {
             }
@@ -440,10 +458,12 @@ const editarCita = (cita) => {
 
 
 
+
+
 const enviarCitaEditada = function () {
     let datosCorrectos = true;
     let fecha = document.querySelector("#fecha");
-    
+
     let observaciones = document.querySelector("#observaciones");
     let textValidacion = document.querySelector("#validacionFormularioEditarCita");
     textValidacion.innerHTML = '';
@@ -458,12 +478,12 @@ const enviarCitaEditada = function () {
 
     let dataCita = {};
 
-    
+
     dataCita.pacienteid = null;
-    dataCita.idEstaus= document.querySelector("#estatus").value;
-    dataCita.idArea= document.querySelector("#areatencion").value;
-    dataCita.fecha= document.querySelector("#fecha").value;
-    dataCita.observaciones= document.querySelector("#observaciones").value;
+    dataCita.idEstaus = document.querySelector("#estatus").value;
+    dataCita.idArea = document.querySelector("#areatencion").value;
+    dataCita.fecha = document.querySelector("#fecha").value;
+    dataCita.observaciones = document.querySelector("#observaciones").value;
 
     if (datosCorrectos) {
         $.ajax({
@@ -476,10 +496,10 @@ const enviarCitaEditada = function () {
                 $('#exampleModal').modal('hide');
             },
             failure: function (response) {
-                
+
             },
             error: function (response) {
-                
+
             }
         });
 
