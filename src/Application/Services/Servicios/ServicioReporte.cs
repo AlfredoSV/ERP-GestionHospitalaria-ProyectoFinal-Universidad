07b4107e5863cfc6c9b0d4b79c1fcd3563e6167a
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-03-25 15:37:23 -0600

    Se agrega IServicioResporte

---
+++ src/Application/Services/Servicios/ServicioReporte.cs
@@ -0,0 +1,30 @@
+﻿using Application.IServicios;
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Layout.Element;
+using iText.Layout;
+
+namespace Application.Servicios
+{
+    public class ServicioReporte : IServicioReporte
+    {
+        public MemoryStream GenerarReporteCitas()
+        {
+            MemoryStream docMemory = new MemoryStream();
+            try
+            {
+                
+            }
+            catch (System.Exception e)
+            {
+
+                throw;
+            }
+          
+
+            return docMemory;
+            
+;            
+        }
+    }
+}
