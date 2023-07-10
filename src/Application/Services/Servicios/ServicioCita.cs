cfa493e930abf478788deef5dae72a80972d1d1b
Alfredo Sánchez Verduzco <AlfredoSanchezVerduzco@outlook.com>
2023-07-09 19:26:58 -0600

    Refactorizzación de CitasController y ServicioCita

--- src/Application/Services/Servicios/ServicioCita.cs
+++ src/Application/Services/Servicios/ServicioCita.cs
@@ -14,63 +14,46 @@ namespace Application.Servicios
     public class ServicioCita : IServicioCitas
     {
         private readonly IRepositorioCitas _repositorioCitas;
+
         public ServicioCita(IRepositorioCitas repositorioCitas)
         {
             _repositorioCitas = repositorioCitas;
         }
 
-        public Cita ConsultarDetalleCita(Guid id)
-        {
-            return _repositorioCitas.ConsultarDetalleCita(id);
-        }
+        public Cita ConsultarDetalleCita(Guid id) => _repositorioCitas.ConsultarDetalleCita(id);
+  
+        public bool CrearNuevaCita(Cita cita) => _repositorioCitas.CrearNuevaCita(cita);
 
-        public bool CrearNuevaCita(Cita cita)
-        {
-            return _repositorioCitas.CrearNuevaCita(cita);
-        }
+        public bool EditarCita( Cita cita)=> _repositorioCitas.EditarCita(cita);
 
-        public bool EditarCita( Cita cita)
-        {
-            return _repositorioCitas.EditarCita(cita);
-        }
-
-        public bool EliminarCita(Guid id)
-        {
-            return _repositorioCitas.EliminarCita(id);
-        }
-
+        public bool EliminarCita(Guid id) => _repositorioCitas.EliminarCita(id);
+  
         public List<Cita> ConsultarCitasPaginadas(int pagina, int tamanioPag,string busqueda,bool ordernado)
         {
             List<Cita> listaCitas = _repositorioCitas.ListarCitasPaginadas(pagina,tamanioPag);
 
-            if(!busqueda.Equals(string.Empty)){
+            if(!busqueda.Equals(string.Empty))
                 listaCitas = listaCitas.Where(c => c.NombrePaciente.Contains(busqueda)).ToList();
-            }
-            if(ordernado){
+            
+            if(ordernado)
                 listaCitas = listaCitas.OrderBy(c => c.NombrePaciente).ToList();
-            }
-
+            
             return listaCitas;
 
         }
 
-        public List<Cita> ConsultarCitas()
-        {
+        public List<Cita> ConsultarCitas() => _repositorioCitas.ListarCitas();
+    
+        public IEnumerable<DtoGrafica> ConsultarCitasGraficas()=>
 
-            return _repositorioCitas.ListarCitas();
-
-        }
-        public IEnumerable<DtoGrafica> ConsultarCitasGraficas()
-        {
-            return _repositorioCitas.ListarCitasGraficas().GroupBy(info => info.EstatusCita)
+            _repositorioCitas.ListarCitasGraficas().GroupBy(info => info.EstatusCita)
                         .Select(group => new DtoGrafica
                         {
                             Metrica = group.Key,
                             Total = group.Count()
                         })
                         .OrderBy(x => x.Metrica); 
-        }
-
+        
 
     }
 }
--- test/Infrastructure.Test/PruebaUnitariaInfraestructura.cs
+++ test/Infrastructure.Test/PruebaUnitariaInfraestructura.cs
@@ -9,57 +9,7 @@ namespace Infrastructure.Test
 {
     public class PruebaUnitariaInfraestructura
     {
-        private readonly RepositorioProductos _productosDbContext;
-        private readonly RepositorioCatalogos _catalogosDbContext;
-        private readonly RepositorioCitas _citasDbContext;
-        private string sqlConeccion = "Data Source=im01.database.windows.net;Initial Catalog=db;Persist Security Info=True;User ID=AdminDb;Password=P@$$w0rd";
-        public PruebaUnitariaInfraestructura()
-        {
-            _productosDbContext = new RepositorioProductos(sqlConeccion);
-            _catalogosDbContext = new RepositorioCatalogos(sqlConeccion);
-            _citasDbContext = new RepositorioCitas(sqlConeccion);
-        }
-        [Fact]
-        public void SeEliminaCorrectamenteProducto()
-        {
-            bool result = _productosDbContext.Delete(Guid.Parse(""));
-            Assert.True(result);
-
-        }
-        [Fact]
-        public void SeConsultaCorrectemneteProducto()
-        {
-            var result = _productosDbContext.Details(Guid.Parse(""));
-            Assert.Equal(result ,new Domain.Producto() { });
-
-        }
-        [Fact]
-        public void SeConsultaEditaCorrectamenteProdcuto()
-        {
-            var result = _productosDbContext.Edit(new Domain.Producto() { });
-            Assert.True(true);
-
-        }
-        [Fact]
-        public void SeEliminaCorrectamenteCita()
-        {
-            bool result = _citasDbContext.Delete(Guid.Parse(""));
-            Assert.True(result);
-
-        }
-        [Fact]
-        public void SeConsultaCorrectemneteCita()
-        {
-            var result = _citasDbContext.Details(Guid.Parse(""));
-            Assert.Equal(result, new Domain.Cita(Guid.Empty, DateTime.Now, Guid.Empty, Guid.Empty,null) { });
-
-        }
-        [Fact]
-        public void SeConsultaEditaCorrectamenteCita()
-        {
-            var result = _citasDbContext.Edit(Guid.Empty, new Domain.Cita(Guid.Empty, DateTime.Now, Guid.Empty, Guid.Empty, null) { });
-            Assert.True(true);
-
-        }
+        
+        
     }
 }
